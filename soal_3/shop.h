68e258600968d58958112ffce4979d1a85b08955
Paundra Pujo Darmawan <92926247+paundraP@users.noreply.github.com>
2025-05-01 17:28:22 +0700

    Update shop.h

--- soal_3/shop.h
+++ soal_3/shop.h
@@ -1,8 +1,6 @@
 #ifndef SHOP_H
 #define SHOP_H
 
-#include "repository.h"
-
 weapon *create(char *name, int damage, int price, char *passive);
 void weapon_list();
 void buy_weapon(player_state *player);
